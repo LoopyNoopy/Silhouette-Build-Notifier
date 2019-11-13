14fd5eff277c0a401e1ba5cef6e20b34a4f13f13
Daniel Burgess <dburgess@silhouettesoftware.com>
2019-11-13 15:50:57 +0000

    Start on C# Version
    Attempt to make what I did in python in C# instead
    
    Reasons:
    -Easier to build an exe (I don't think people will be happy running it though visual studio)
    - Easier to add a systray icon (Very broad term of the word "easier")

---
+++ Silhouette Build Notifier CSharp/FrmNotifierUI.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Silhouette_Build_Notifier_CSharp
+{
+    partial class FrmNotifierUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmNotifierUI));
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(214, 25);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Silhouette Build Notifier";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.Location = new System.Drawing.Point(59, 25);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(0, 21);
+            this.lblDate.TabIndex = 1;
+            // 
+            // FrmNotifierUI
+            // 
+            this.AccessibleDescription = "Menu for the Silhouette Build Notifier, use this window to select which branches " +
+    "you would like the program to watch";
+            this.AccessibleName = "Silhouette Build Notifier Menu";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblTitle);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmNotifierUI";
+            this.Text = "Build Notifier";
+            this.Load += new System.EventHandler(this.FrmNotifierUI_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblDate;
+    }
+}
+
---
+++ Silhouette Build Notifier CSharp/FrmNotifierUI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Silhouette_Build_Notifier_CSharp
+{
+    public partial class FrmNotifierUI : Form
+    {
+        public FrmNotifierUI()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmNotifierUI_Load(object sender, EventArgs e)
+        {
+            lblDate.Text = DateTime.Now.ToString("MMM") + " " + DateTime.Now.Year.ToString();
+        }
+    }
+}
