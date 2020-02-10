e8e6b33b7edea5acd9a6775536d5ba797d3da4c9
Loopy Noopy <danieljbburgess@hotmail.co.uk>
2020-02-10 23:21:53 +0000

    Inital test of a working Toast Notification - Some concerns --On personal PC it screamed about missing references, after it was installed and restarted it worked fine. It did show a warning that it wasn't looking in the right place for the notficiation package but once built it worked fine --Now I need to convert what I did in the C# project over to this one -> Should be easy -> The .resx file might be a tad tricky

--- SBNUWP/SBN UWP/MainPage.xaml.cs
+++ SBNUWP/SBN UWP/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.Toolkit.Uwp.Notifications;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +27,50 @@ namespace SBN_UWP
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var toastContent = new ToastContent()
+            {
+                Visual = new ToastVisual()
+                {
+                    BindingGeneric = new ToastBindingGeneric()
+                    {
+                        Children =
+            {
+                new AdaptiveText()
+                {
+                    Text = "New Build alert"
+                },
+                new AdaptiveText()
+                {
+                    Text = "Silhouette Studio 4.4.041 has been detected"
+                },
+                new AdaptiveText()
+                {
+                    Text = "Would you like to download this build?"
+                }
+            }
+                    }
+                },
+                Actions = new ToastActionsCustom()
+                {
+                    Buttons =
+        {
+            new ToastButtonSnooze("Yes"),
+            new ToastButtonDismiss("No")
+        }
+                },
+                Launch = "action=viewEvent&eventId=1983",
+                Scenario = ToastScenario.Reminder
+            };
+
+            // Create the toast notification
+            var toastNotif = new ToastNotification(toastContent.GetXml());
+
+            // And send the notification
+            ToastNotificationManager.CreateToastNotifier().Show(toastNotif);
         }
     }
 }
